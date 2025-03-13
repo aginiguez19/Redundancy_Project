b411ddaaf5c436dc16299982f6d47c00fcc4c51b
Abraham Gumaro Iniguez <138175973+aginiguez19@users.noreply.github.com>
2025-03-12 17:28:15 -0700

    First version of Correlation Mat Function

--- Correlation Matri Functionx.R
+++ Correlation Matri Functionx.R
@@ -2,47 +2,40 @@
 
 
 # Correlation Matrix Function ---------------------------------------------
-
-
-
-
 library(matrixcalc)
 
-I <- diag(1,3)
-eigen(I)
-matrix <- matrix(1, 3,3)
-eigen(matrix)$values >= 0
-i12s.positive.semi.definite()
+
 
 EF <- .5
 nv <- 4
-diag_mat <- diag(1, nv)
-off_diag_elements <- diag_mat[lower.tri(diag_mat)]
-n_elements <- length(off_diag_elements)
-n_elements
-edge_vector <- rbinom(n_elements, 1, 0.5)
-edge_vector <- replace(edge_vector, edge_vector == 1, EF)
-edge_vector
+edge.probability <- 0.5
 
 
-corr.gen = function(nv, EF = 0.50, edge.prob = 0.50){
-  # Define a flag variable to stop your loop
+flag = 0
+corr.gen = function(nv, EF, edge.probability){
   while(flag < 1){
     diag_mat <- diag(1,nv)
     off_diag_elements <- diag_mat[lower.tri(diag_mat)]
     n_elements <- length(off_diag_elements)
-    edge_vector <- rbinom(n = n_elements, size = 1, p = edge.prob)
+    edge_vector <- rbinom(n = n_elements, size = 1, prob = edge.probability)
     edge_vector <- replace(edge_vector, edge_vector == 1, EF)
     diag_mat[lower.tri(diag_mat)] <- edge_vector
-    
-    
-    
-    
-# Populate the lower.triangular and reflect that to the upper.triangular to make a symmetric correlation matrix
-# Check if the matrix you made is PSD; if so, set flag to 1 otherwise, flag stays 0 and keeps going
+    diag_mat[upper.tri(diag_mat)] <- t(diag_mat)[upper.tri(diag_mat)]
+    return(diag_mat)
+    if(is.positive.semi.definite(diag_mat) == T){
+      flag = 1
+    }
   }
-  return(correlation.matrix)
 }
+
+
+
+# Check if the matrix you made is PSD; if so, set flag to 1 otherwise, flag stays 0 and keeps going
+
+
+
+
+
 
 flag = 0
 while(flag < 1){
@@ -54,7 +47,6 @@ while(flag < 1){
 }
 
 
-diag_mat
 
  
 
