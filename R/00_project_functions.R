d8cadc2169346e358f87ab59447271a4d4f2a52b
Abraham Gumaro Iniguez <138175973+aginiguez19@users.noreply.github.com>
2025-08-29 15:34:48 -0700

    Changes

--- R/00_project_functions.R
+++ R/00_project_functions.R
@@ -38,8 +38,8 @@ cor2pcor <- function(matrix){
 
 
 
-# Form 1 HCSF Function  ----------------------------------------------------------
-hcsf <- function(matrix, loading = .9){
+# Correlation of Indicators + Redundant Indicator Function  ----------------------------------------------------------
+ind_corr <- function(matrix, loading, scalar = 1){
   # Lambda
   dimensions <- dim(matrix)
   lambda_matrix <- diag(x = loading, nrow = dimensions[1] + 1 , ncol = dimensions[2])
@@ -47,32 +47,18 @@ hcsf <- function(matrix, loading = .9){
   lambda_matrix[last_element] <- loading
   
   # Lambda * Psi * t(Lambda)
-  redun_mat <- lambda_matrix %*% true_mat %*% t(lambda_matrix)
+  redun_mat <- lambda_matrix %*% matrix %*% t(lambda_matrix)
   
   # Theta
   dimensions_theta <- dim(redun_mat)
-  theta <- diag(.19, nrow = dimensions_theta[1], ncol = dimensions_theta[2])
+  theta <- diag((x = 1 - (loading)^2), nrow = dimensions_theta[1], ncol = dimensions_theta[2])
   redun_mat <- redun_mat + theta
+  redun_mat[1:nv,nv+1] <- scalar*redun_mat[1:nv,nv+1]
   return(redun_mat)
 }
 
 
-# Form 2 HCDF Function ----------------------------------------------------
 
-hcdf <- function(matrix, loading = .9){
-  # Lambda
-  dimensions <- dim(matrix)
-  lambda_matrix <- diag(x = loading, nrow = dimensions[1] + 1 , ncol = dimensions[2])
-  last_element <- nrow(lambda_matrix) * ncol(lambda_matrix)
-  lambda_matrix[last_element] <- loading
-  
-  # Lambda * Psi * t(Lambda)
-  redun_mat <- lambda_matrix %*% true_mat %*% t(lambda_matrix)
-  
-  # Theta
-  dimensions_theta <- dim(redun_mat)
-  theta <- diag(.19, nrow = dimensions_theta[1], ncol = dimensions_theta[2])
-  redun_mat <- redun_mat + theta
-  redun_mat[1:nv,nv+1] <- -1*redun_mat[1:nv,nv+1]
-  return(redun_mat)
-}
+
+
+
