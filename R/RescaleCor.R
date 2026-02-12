648592f07bd31c2467a28ef9c79e4baf75e4b2da
Abraham Gumaro Iniguez <138175973+aginiguez19@users.noreply.github.com>
2026-02-12 09:01:45 -0800

    Plots

--- R/RescaleCor.R
+++ R/RescaleCor.R
@@ -1,26 +1,29 @@
 cor.gen = function(nvar = NULL,
                    bg.cor = 0.30,
                    mn.cor = 0.50,
-                   prop.cor = 0.50,
+                   prop.cor = 1,
                    iter.lim = 100,
-                   sd = 0.25,
+                   sd = 0.1,
                    tol.eig = 1e-8,
                    mean.tol = 0.02,
                    clip = 0.99){
   
+
+  
   flag = 0
   iter = 0
   
-  n_lower = (nvar^2 - nvar) / 2
-  n.big = floor(n_lower * prop.cor)
   
+  n_lower = (nvar^2 - nvar) / 2 # Unique off-diagonal elements 
+  n.big = floor(n_lower * prop.cor) # floor will never be greater than itself, 10.5 = 10
+  # take # of unique off-diag elements and * proportion of correlations for n.big so how many will be larger correlations 
   while(flag < 1){
     iter = iter + 1
     
-    temp = matrix(rnorm(nvar^2, mean = bg.cor, sd = sd), nvar, nvar)
-    diag(temp) = 1
+    temp = matrix(rnorm(nvar^2, mean = bg.cor, sd = sd), nvar, nvar) # Initial correlation matrix 
+    diag(temp) = 1 # Set diagonals to 1 to be correlations 
     
-    idx.lower = which(lower.tri(temp))
+    idx.lower = which(lower.tri(temp)) # R column-major indexing 
     
     if (n.big > 0) {
       idx.big = sample(idx.lower, n.big)
@@ -30,32 +33,35 @@ cor.gen = function(nvar = NULL,
     }
     
     temp[lower.tri(temp)] = pmax(pmin(temp[lower.tri(temp)], clip), -clip)
-    
+    # any values less than or greater than clip will be replaced by clip, making the correlations bounded between -1 and 1
     temp[upper.tri(temp)] = t(temp)[upper.tri(temp)]
+    # Replace lower elements with upper
     diag(temp) = 1
     
-    E = eigen(temp, symmetric = TRUE)
+    # Eigen check
+    E = eigen(temp, symmetric = TRUE) # Provides eigen values and vectors, eigen decomposition 
+    # t(E$vectors) %*% E$vectors will give the identity matrix, all eigenvalues must be >= 0
     vals = E$values
-    vals[vals < tol.eig] = tol.eig
+    vals[vals < tol.eig] = tol.eig # Replace any with a really small positive value that are less than 0
     temp.psd = E$vectors %*% (diag(vals, nvar, nvar)) %*% t(E$vectors)
-    
-    d = sqrt(diag(temp.psd))
+    # Rebuild the matrix that is psd 
+    d = sqrt(diag(temp.psd)) # Ensure it is correlation matrix
     temp_cor = temp.psd / outer(d, d)
     diag(temp_cor) = 1
     
     if (length(idx.big) > 0) {
-      big.mean = mean(temp_cor[idx.big])
+      big.mean = mean(temp_cor[idx.big]) # Average correlation size of larger correlations 
     } else {
       big.mean = NA_real_
     }
-    
+    setdiff(idx.lower, idx.big) # Compute mean for baseline correlations 
     idx.bg = setdiff(idx.lower, idx.big)
     if (length(idx.bg) > 0) {
       bg.mean = mean(temp_cor[idx.bg])
     } else {
       bg.mean = NA_real_
     }
-    
+
     cat("\r", paste0("Iter ", iter,
                      " | big mean=", ifelse(is.na(big.mean), "NA", round(big.mean, 3)),
                      " | bg mean=", ifelse(is.na(bg.mean), "NA", round(bg.mean, 3))), sep = "")
@@ -82,16 +88,19 @@ cor.gen = function(nvar = NULL,
   return(NULL)
 }
 
-x = cor.gen(nvar = 10, 
+x = cor.gen(nvar = 3, 
             bg.cor = 0.30, 
             mn.cor = 0.50, 
             prop.cor = 1, 
             iter.lim = 50000, 
-            sd = 0.25, 
+            sd = 0.5, 
             tol.eig = 1e-8, 
             mean.tol = 0.04, 
             clip = 0.99)
-x
+
+
+
+
 min(Re(eigen(x)$values)) > 0 & isSymmetric(x)
 hist(x[x!=1])
 mean(x[x!=1]); sd(x[x!=1])
